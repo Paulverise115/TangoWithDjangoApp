b78dc74087224e49d095db45ed4809b68a2d3683
Paul Burns <paulburns115@googlemail.com>
2020-04-02 14:33:37 +0100

    added media paths

--- workspace/tango_with_django_project/tango_with_django_project/settings.py
+++ workspace/tango_with_django_project/tango_with_django_project/settings.py
@@ -19,6 +19,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -126,3 +128,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 
+# Media files
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
