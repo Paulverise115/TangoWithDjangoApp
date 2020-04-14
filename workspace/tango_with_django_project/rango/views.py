c41ef16990918a92836cbb71c677793d1e1fc478
Paul Burns <paulburns115@googlemail.com>
2020-04-14 16:21:48 +0100

    update index.html with new categories

--- workspace/tango_with_django_project/rango/views.py
+++ workspace/tango_with_django_project/rango/views.py
@@ -4,8 +4,9 @@ from rango.models import Category
 
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {}
-    context_dict['boldmessage'] =  'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context=context_dict)
 
