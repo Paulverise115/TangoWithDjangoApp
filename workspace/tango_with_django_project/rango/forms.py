9c0dc5d5e22ee7b0dd65aaaed5e98e96ae56f354
Paul Burns <paulburns115@googlemail.com>
2020-04-16 20:07:46 +0100

    added forms for userform and userprofileform

--- workspace/tango_with_django_project/rango/forms.py
+++ workspace/tango_with_django_project/rango/forms.py
@@ -32,3 +32,17 @@ class PageForm(forms.ModelForm):
         return cleaned_data
 
 
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('website', 'picture',)
+
+
