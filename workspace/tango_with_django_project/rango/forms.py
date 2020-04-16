9b9531d224dcf4cbb0be37f894f6d1b31a3797ec
Paul Burns <paulburns115@googlemail.com>
2020-04-16 20:08:57 +0100

    added forms for userform and userprofileform

--- workspace/tango_with_django_project/rango/forms.py
+++ workspace/tango_with_django_project/rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
 
 
 class CategoryForm(forms.ModelForm):
