cc06c0b5a5420bd28b65d033e89e35827f897cba
Paul Burns <paulburns115@googlemail.com>
2020-04-17 13:10:59 +0100

    start chapt 10

--- workspace/tango_with_django_project/rango/views.py
+++ workspace/tango_with_django_project/rango/views.py
@@ -11,7 +11,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 
-
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
