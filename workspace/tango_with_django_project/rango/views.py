a095f32c7a6bac34358648ca5ec001b59abb6f08
Paul Burns <paulburns115@googlemail.com>
2020-04-17 13:20:48 +0100

    added cookie to view

--- workspace/tango_with_django_project/rango/views.py
+++ workspace/tango_with_django_project/rango/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def index(request):
@@ -151,3 +152,17 @@ def user_logout(request):
     logout(request)
 
     return redirect(reverse('rango:index'))
+
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.striptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits +1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
